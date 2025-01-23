430b409609081373e291358c2d8aabbddf5065b3
Juan Jose Gallego <jjosegallegomesa@gmail.com>
2025-01-23 16:44:42 -0500

    se crea proyecto base

---
+++ src/main/java/com/example/ProyectoSpringboot/ProyectoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProyectoSpringboot/ProyectoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
