add639e7814ef2240167b02837e3400a6ad6b156
Juan Jose Gallego <jjosegallegomesa@gmail.com>
2025-01-27 18:10:35 -0500

    se crean servicios asociados a la entidad usuario

---
+++ src/main/java/com/example/ProyectoSpringboot/controladores/ControladorUsuario.java
@@ -0,0 +1,16 @@
+package com.example.ProyectoSpringboot.controladores;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+public class ControladorUsuario {
+
+    //inyectar dependencia al servicio
+
+    //listar los metodos para responder
+    //y atender cada uno de los servicios
+    //programados
+
+}
