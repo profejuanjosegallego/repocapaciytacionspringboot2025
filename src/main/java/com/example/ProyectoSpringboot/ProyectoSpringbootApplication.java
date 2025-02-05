d95af525f0de14f71d2740108ac3fc7e4bc800e7
Juan Jose Gallego <jjosegallegomesa@gmail.com>
2025-02-05 07:16:00 -0500

    se crea controlador y se activan cors

--- src/main/java/com/example/ProyectoSpringboot/ProyectoSpringbootApplication.java
+++ src/main/java/com/example/ProyectoSpringboot/ProyectoSpringbootApplication.java
@@ -2,12 +2,28 @@ package com.example.ProyectoSpringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProyectoSpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoSpringbootApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")  // Permite todas las rutas
+						.allowedOrigins("*")  // Permite cualquier origen
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Métodos permitidos
+						.allowedHeaders("*");  // Permite todos los encabezados
+			}
+		};
 	}
 
 }
